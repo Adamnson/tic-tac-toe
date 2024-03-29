b9b5db2930fcacba96349d3b7a2945792d84dcc9
Adamnson-0dinson <theawakeavatar@gmail.com>
2024-03-29 19:19:28 +0530

    implemented Player class

--- game.rb
+++ game.rb
@@ -36,7 +36,8 @@
 require 'pry-byebug'
 
 LINES = [[1,2,3], [4,5,6], [7,8,9], [1,4,7], [2,5,8], [3,6,9], [1,5,9], [3,5,7]]
-
+VALID_X = ['X', 'x', '*']
+VALID_O = ['O', 'o', '0']
 class Game
   attr_reader :arr
 
@@ -67,15 +68,41 @@ class Game
       end
     end 
 
-    def add_mark(sign, pos)
+    def add_mark(player, pos)
       if valid_pos?(pos)
-        @arr[pos-1] = sign
+        @arr[pos-1] = player.sign
       else
-        puts "Position choice invalid"
+        player.is_computer ? self.add_mark(player, player.choose_pos()) : (puts "Position choice invalid")
       end
     end
 
 end
+
+class Player
+  attr_accessor :sign, :is_computer
+
+    def initialize()
+      is_computer = false
+    end
+
+  # private
+    def choose_pos()
+      1+rand(8)
+    end
+
+    def self.sign_assignment(sign, player1, player2)
+      if VALID_X.any?(sign)
+        player1.sign = sign
+        player2.sign = VALID_O[rand(2)]
+      else 
+        puts "Invalid choice" unless sign == 0
+        player1.sign = 0
+        player2.sign = VALID_X[rand(2)]
+      end
+    end
+
+end
+
 
 vanilla = Game.new([1,2,3,4,5,6,7,8,9])
 row1x   = Game.new(['X','X','X',4,5,6,7,8,9])
@@ -88,14 +115,31 @@ col10   = Game.new([0,2,3,0,5,6,0,8,9])
 dig10   = Game.new([0,2,3,4,0,6,7,8,0])
 dig20   = Game.new([1,2,0,4,0,6,0,8,9])
 
-row10.draw_grid()
-puts "Enter the position: "
-pos = gets.chomp.to_i
+player = Player.new
+computer = Player.new
+computer.is_computer = true
 
-p "you entered #{pos}"
-row10.add_mark('X', pos)
-row10.draw_grid()
+print " X or 0 ?:  "
+sign = gets.chomp
+Player.sign_assignment(sign, player, computer)
+puts "The player is #{player.sign}"
+puts "The computer is #{computer.sign}"
 
+3.times {
+  vanilla.add_mark(computer, computer.choose_pos())
+  vanilla.draw_grid()
+
+  # row10.draw_grid()
+  puts "Enter the position: "
+  pos = gets.chomp.to_i
+
+  # p "you entered #{pos}"
+  vanilla.add_mark(player, pos)
+  vanilla.draw_grid()
+}
+
+
+#handle when computer chooses invalid position
 
 =begin
 
