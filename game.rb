6bd3ca6e4951a3cb8ba0174b31aa16523fb17bb7
Adamnson-0dinson <theawakeavatar@gmail.com>
2024-04-01 11:37:53 +0530

    implement PvP logic, celebration message, bugfix
    - the game runs in a loop until a winner emerges
    - on winning, a celebratory message is displayed
      for winner and a better luck next time message
      is shown for the other player
    - valid_pos includes a check to ensure that the
      position input by user is within [1,9] valid pos
      range

--- game.rb
+++ game.rb
@@ -55,20 +55,17 @@ class Game
     end
 
     def valid_pos?(pos)
-      if ( (@arr[pos-1] != 'X') and (@arr[pos-1] != 0) )
-        return true
+      if (pos >=1 and pos <=9)  
+        if ( (@arr[pos-1] != 'X') and (@arr[pos-1] != 0) )
+          return true
+        end
       end
       false
     end
 
-    def won?(sign)
-      LINES.any? do |line| 
-      line.all? {|pos| @arr[pos-1] == sign}
-      end
-    end 
-
     def add_mark(player, pos)
-      if valid_pos?(pos)
+      player.valid_pos = valid_pos?(pos)
+      if player.valid_pos
         @arr[pos-1] = player.sign
       else
          puts "Position choice invalid"
@@ -78,10 +75,29 @@ class Game
 end
 #*******************************************
 class Player
-  attr_accessor :sign, :is_computer
+  attr_accessor :sign, :valid_pos
 
     def initialize(sign)
       @sign = sign
+      @valid_pos = false
+    end
+
+    def reset_pos()
+      @valid_pos = false
+    end
+
+    def won?(board)
+      LINES.any? do |line| 
+      line.all? {|pos| board.arr[pos-1] == self.sign}
+      end
+    end 
+
+    def celebrate()
+      puts "#{self.sign} wins"
+    end
+
+    def better_luck()
+      puts "Better luck nex time #{self.sign}"
     end
 end
 
@@ -89,31 +105,42 @@ end
 # main()
 #*******************************************
 
-vanilla = Game.new([1,2,3,4,5,6,7,8,9])
-row1x   = Game.new(['X','X','X',4,5,6,7,8,9])
-row2x   = Game.new([1,2,3,'X','X','X',7,8,9])
-row3x   = Game.new([1,2,3,4,5,6,'X','X','X'])
-row10   = Game.new([0,0,0,4,5,6,7,8,9])
-row20   = Game.new([1,2,3,0,0,0,7,8,9])
-row30   = Game.new([1,2,3,4,5,6,0,0,0])
-col10   = Game.new([0,2,3,0,5,6,0,8,9])
-dig10   = Game.new([0,2,3,4,0,6,7,8,0])
-dig20   = Game.new([1,2,0,4,0,6,0,8,9])
-
-board = vanilla
+board = Game.new([1,2,3,4,5,6,7,8,9])
 
 player1 = Player.new("X")
 player2 = Player.new(0)
 
-board.draw_grid()
-puts "Player 1, select your position: "
-pos = gets.chomp.to_i
-board.add_mark(player1, pos)
-board.draw_grid()
-puts "Player 2, your turn: "
-pos = gets.chomp.to_i
-board.add_mark(player2, pos)
-board.draw_grid()
+
+until player1.won?(board) or player2.won?(board) do 
+  board.draw_grid()
+  until player1.valid_pos do
+    puts "Player 1, select your position: "
+    pos = gets.chomp.to_i
+    board.add_mark(player1, pos)
+  end
+  if player1.won?(board)
+    board.draw_grid()
+    player1.celebrate()
+    player2.better_luck()
+    break
+  end
+  board.draw_grid()
+   until player2.valid_pos do
+    puts "Player 2, your turn: "
+    pos = gets.chomp.to_i
+    board.add_mark(player2, pos)
+  end
+  if player2.won?(board)
+    board.draw_grid()
+    player1.better_luck()
+    player2.celebrate()
+    break
+  end
+  player1.reset_pos()
+  player2.reset_pos()
+end
+
+
 
 
 =begin
